f06e13898b14c501af12fea6b4b0ad7baf52bad2
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2023-07-03 01:22:55 +0530

    added HttpException.

--- BlogApp/main.py
+++ BlogApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 import models
 import schemas
 import database
@@ -17,7 +17,7 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=database.engine)
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -26,7 +26,15 @@ def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
+@app.get("/blog", status_code=status.HTTP_200_OK)
 def get_blogs(db: Session = Depends(get_db)):
     new_blogs = db.query(models.Blog).all()
     return new_blogs
+
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def get_post_by_id(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"no blog found with id {id}.")
+    return blog
