078b84eba9c7fe170b2f7426c14af0754abfb2b0
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2023-07-03 01:08:25 +0530

    created post and get endpoints.

--- BlogApp/database.py
+++ BlogApp/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DB_URL = 'sqlite:///blog.db'
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+SQLALCHEMY_DB_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False})
-LocalSession = sessionmaker(bind=engine, autoflush=False, autocommit=False,)
+LocalSession = scoped_session(sessionmaker(bind=engine, autoflush=False, autocommit=False, ))
+LocalSession()
 Base = declarative_base()
--- BlogApp/main.py
+++ BlogApp/main.py
@@ -1,4 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends
+import models
+import schemas
+import database
+from sqlalchemy.orm import Session
+
+
+async def get_db():
+    db = database.LocalSession
+    try:
+        yield db
+    finally:
+        db.close_all()
+
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=database.engine)
+
+
+@app.post("/blog")
+def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blog")
+def get_blogs(db: Session = Depends(get_db)):
+    new_blogs = db.query(models.Blog).all()
+    return new_blogs
