2f469b9c141f9bda148bd4c0fab59e7ac5dcc029
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2023-07-21 18:27:49 +0530

    added routes again.

--- E-Commerce-App/main.py
+++ E-Commerce-App/main.py
@@ -7,7 +7,6 @@ app.include_router(users.router)
 app.include_router(products.router)
 app.include_router(orders.router)
 
-
 @app.get("/")
 async def root():
     return {"message": "Welcome To E Commerce App!"}
